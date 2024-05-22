6de724ee7b06127a0261d6c2a8ac21feaf5419ac
unknown <kkning@ucdavis.edu>
2024-05-22 01:28:31 -0700

    update

---
+++ ecs116_a2.sql
@@ -0,0 +1,42 @@
+set search_path to public
+
+--step3a
+alter table reviews 
+add column datetime timestamp;
+
+UPDATE reviews
+SET datetime = to_timestamp((TO_CHAR(date::date, 'YYYY-MM-DD') || ' 12:00:00'), 'YYYY-MM-DD hh24:mi:ss');
+
+with yrs as 
+(
+select datetime, cast(date_part('Year', datetime) as varchar) as yr
+from reviews
+)
+select count(datetime), yr
+from yrs
+group by yr
+order by yr
+
+--step3b
+alter table reviews 
+add column comments_tsv tsvector;
+
+update reviews 
+set comments_tsv = to_tsvector(comments);
+
+create index if not exists comments_tsv_in_reviews
+on reviews using GIN(comments_tsv);
+
+--with index
+select count(*)
+from reviews r
+where comments_tsv @@ to_tsquery('awesome')
+and datetime >='2023-01-01'
+and datetime <='2023-12-31';
+
+--without index
+select count(*)
+from reviews r
+where comments ilike '%awesome%'
+and datetime >='2023-01-01';
+and datetime <='2023-12-31'; 
